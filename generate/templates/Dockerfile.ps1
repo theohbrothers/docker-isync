1dbf749f2f08d337c5a76bd261951bcf3b9478ef
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-04-19 17:42:02 +0000

    Refactor: Generate Dockerfiles with hardcoded checksums for repeatable builds

--- generate/templates/Dockerfile.ps1
+++ generate/templates/Dockerfile.ps1
@@ -32,57 +32,31 @@ RUN set -eux; \
 
 foreach ($c in $VARIANT['_metadata']['components']) {
     if ($c -eq 'pingme') {
-@'
-# Install pingme
-RUN set -eux; \
-    export PINGME_VERSION="0.2.5"; \
-    OS=$( uname -o ); \
-    # The naming conventions of the binaries is not consistent, hence the need for ARCH workaround
-    ARCH=$( \
-        if [ "$TARGETARCH" = 'amd64' ]; then \
-            uname -m; \
-        elif [ "$TARGETARCH" = '386' ]; then \
-            echo "i$TARGETARCH"; \
-        else \
-            echo "$TARGETARCH"; \
-        fi; \
-    ); \
-    FILE=pingme_${OS}_${ARCH}${TARGETVARIANT}.tar.gz; \
-    wget https://github.com/kha7iq/pingme/releases/download/v$PINGME_VERSION/pingme_checksums.txt; \
-    wget -q https://github.com/kha7iq/pingme/releases/download/v${PINGME_VERSION}/$FILE; \
-    cat pingme_checksums.txt | grep "$FILE" | sha256sum -c -; \
-    tar -xvf "$FILE" -- pingme LICENSE.md; \
-    chmod +x pingme; \
-    mv pingme /usr/local/bin/pingme; \
-    mv LICENSE.md /usr/local/bin/pingme.LICENSE; \
-    pingme --version | grep "$PINGME_VERSION"; \
-    rm -f pingme_checksums.txt; \
-    rm -f "$FILE";
-
-
-'@
+        Generate-DownloadBinary @{
+            project = 'github.com/kha7iq/pingme'
+            version = 'v0.2.5'
+            binary = 'pingme'
+            archiveformat = '.tar.gz'
+            archivefiles = @(
+                'pingme'
+                'LICENSE.md'
+            )
+            checksums = 'pingme_checksums.txt'
+            architectures = $VARIANT['_metadata']['platforms']
+            versionSubcommand = '--version'
+        }
     }
 
     if ($c -eq 'restic') {
-@'
-# Install restic
-# These packages are needed for all restic features to work. See: https://github.com/restic/restic/blob/0.15.1/docker/Dockerfile
-RUN apk add --update --no-cache ca-certificates fuse openssh-client tzdata jq
-RUN set -eux; \
-    RESTIC_VERSION=0.15.1; \
-    FILE=restic_${RESTIC_VERSION}_${TARGETOS}_${TARGETARCH}.bz2; \
-    wget -q https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/$FILE; \
-    wget -q https://github.com/restic/restic/releases/download/v${RESTIC_VERSION}/SHA256SUMS; \
-    SHA=$( sha256sum "$FILE" ); \
-    cat SHA256SUMS | grep "$FILE" | sha256sum -c -; \
-    rm -f SHA256SUMS; \
-    bzip2 -d "$FILE"; \
-    mv restic_${RESTIC_VERSION}_${TARGETOS}_${TARGETARCH} /usr/local/bin/restic; \
-    chmod +x /usr/local/bin/restic; \
-    restic version | grep "^restic $RESTIC_VERSION";
-
-
-'@
+        Generate-DownloadBinary @{
+            project = 'github.com/restic/restic'
+            version = 'v0.15.1'
+            binary = 'restic'
+            archiveformat = '.bz2'
+            checksums = 'SHA256SUMS'
+            architectures = $VARIANT['_metadata']['platforms']
+            versionSubcommand = 'version'
+        }
     }
 }
 
